3a654b8dbd7a9b90bc056f36b4813be342bd3b96
Fábio Vedovelli <vedovelli@gmail.com>
2016-02-28 12:39:34 -0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+
+  import SelectionBox from './components/selection-box.vue'
+
+  export default {
+    data () {
+      return {
+        selectionBoxOptions: ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis'],
+        selectionBoxSelected: ['dois', 'quatro', 'tres', 'seis'],
+      }
+    },
+    components: { SelectionBox },
+
+    methods: {
+      selectedHandler (obj) {
+        window.console.log(JSON.stringify(obj))
+      }
+    }
+  }
+</script>
+
+
+<template>
+  <div>
+    <selection-box
+      @selected="selectedHandler"
+      name = "numeros"
+      :options = "selectionBoxOptions"
+      :selected = "selectionBoxSelected">
+    </selection-box>
+  </div>
+</template>
+
